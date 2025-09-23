d5a1d59dfea029701ac43d29fb8b6fef443f7c5d
EidenFox <3rcafe@gmail.com>
2025-09-22 21:34:34 -0300

    alterações locais antes de fetch

--- Gerenciador POO/src/Main.java
+++ Gerenciador POO/src/Main.java
@@ -11,19 +11,19 @@ public class Main {
 
         loginDao.cadastrar(new Login(1, "admin", "admin@admin.com", "admin", "18/08/2025", ""));
 
-        /*  //carga de sistema
-        loginDao.cadastrar(new Login("Daniel", "3rcafe@gmail.com", "senha123", "18/08/2025", ""));
-        loginDao.cadastrar(new Login("Alice", "alice@example.com", "senha123", "01/01/2025", ""));
-        loginDao.cadastrar(new Login("Bruno", "bruno@example.com", "123456", "15/02/2025", ""));
-        loginDao.cadastrar(new Login("Carla", "carla@example.com", "abc123", "20/03/2025", ""));
-        loginDao.cadastrar(new Login("Diego", "diego@example.com", "senhaSegura!", "05/04/2025", ""));
-        loginDao.cadastrar(new Login("Eva", "eva@example.com", "minhaSenha", "10/05/2025", ""));
-        loginDao.cadastrar(new Login("Fernando", "fernando@example.com", "senha123", "25/06/2025", ""));
-        loginDao.cadastrar(new Login("Giulia", "giulia@example.com", "senha456", "30/07/2025", ""));
-        loginDao.cadastrar(new Login("Henrique", "henrique@example.com", "pass789", "12/08/2025", ""));
-        loginDao.cadastrar(new Login("Isabela", "isabela@example.com", "senha321", "18/08/2025", ""));
-        loginDao.cadastrar(new Login("João", "joao@example.com", "joaoSenha", "18/08/2025", ""));
-        */
+//        /*  //carga de sistema
+        loginDao.cadastrar(new Login(2, "Daniel", "3rcafe@gmail.com", "senha123", "18/08/2025", ""));
+        loginDao.cadastrar(new Login(3, "Alice", "alice@example.com", "senha123", "01/01/2025", ""));
+        loginDao.cadastrar(new Login(5, "Bruno", "bruno@example.com", "123456", "15/02/2025", ""));
+        loginDao.cadastrar(new Login(6, "Carla", "carla@example.com", "abc123", "20/03/2025", ""));
+        loginDao.cadastrar(new Login(7, "Diego", "diego@example.com", "senhaSegura!", "05/04/2025", ""));
+        loginDao.cadastrar(new Login(8, "Eva", "eva@example.com", "minhaSenha", "10/05/2025", ""));
+        loginDao.cadastrar(new Login(9, "Fernando", "fernando@example.com", "senha123", "25/06/2025", ""));
+        loginDao.cadastrar(new Login(10, "Giulia", "giulia@example.com", "senha456", "30/07/2025", ""));
+        loginDao.cadastrar(new Login(11, "Henrique", "henrique@example.com", "pass789", "12/08/2025", ""));
+        loginDao.cadastrar(new Login(12, "Isabela", "isabela@example.com", "senha321", "18/08/2025", ""));
+        loginDao.cadastrar(new Login(13, "João", "joao@example.com", "joaoSenha", "18/08/2025", ""));
+//        */
 
         boolean continuar = false;
         int op;
@@ -36,8 +36,8 @@ public class Main {
         System.out.println("[1] Cadastro de Usuários.");
         System.out.println("[2] Listar Usuários.");
         System.out.println("[3] Buscar Usuário.");
-        System.out.println("[4] .");
-        System.out.println("[5] Sair.");
+        System.out.println("[4] Alterar Registro.");
+        System.out.println("[0] Sair.");
         op = scan.nextInt();
         scan.nextLine();
 
@@ -101,12 +101,34 @@ public class Main {
                     case "nome":
                         System.out.println("Digite o nome que deseja buscar: ");
                         login = loginDao.buscarPorNome(scan.nextLine().toLowerCase());
-                        System.out.println(login);
+                        if (login != null) {
+                            System.out.println("------------------------");
+                            System.out.println("ID: " + login.getID());
+                            System.out.println("Nome: " + login.getNome());
+                            System.out.println("E-mail: " + login.getEmail());
+                            System.out.println("Senha: " + login.getSenha());
+                            System.out.println("Status: " + login.isStatus());
+                            System.out.println("Data Cadastro: " + login.getDataCadastro());
+                            System.out.println("Data da Ultima Atualização: " + login.getDataAtualizacao());
+                            System.out.println("------------------------");
+                        }
+//                        System.out.println(login);
                         break;
                     case "id":
                         System.out.println("Digite o ID que deseja buscar: ");
                         login = loginDao.buscarPorId(scan.nextInt());
-                        System.out.println(login);
+                        if (login != null) {
+                            System.out.println("------------------------");
+                            System.out.println("ID: " + login.getID());
+                            System.out.println("Nome: " + login.getNome());
+                            System.out.println("E-mail: " + login.getEmail());
+                            System.out.println("Senha: " + login.getSenha());
+                            System.out.println("Status: " + login.isStatus());
+                            System.out.println("Data Cadastro: " + login.getDataCadastro());
+                            System.out.println("Data da Ultima Atualização: " + login.getDataAtualizacao());
+                            System.out.println("------------------------");
+                        }
+//                        System.out.println(login);
                         break;
                     default:
                         System.out.println("Opção Não Inválida, Tente Novamente!\n");
@@ -114,8 +136,62 @@ public class Main {
                 }
                 break;
             case 4:
+                System.out.println("Digite o ID do usuário que deseja alterar: ");
+                login = loginDao.buscarPorId(scan.nextInt());
+                if (login != null) {
+                    System.out.println("------------------------");
+                    System.out.println("ID: " + login.getID());
+                    System.out.println("Nome: " + login.getNome());
+                    System.out.println("E-mail: " + login.getEmail());
+                    System.out.println("Senha: " + login.getSenha());
+                    System.out.println("Status: " + login.isStatus());
+                    System.out.println("Data Cadastro: " + login.getDataCadastro());
+                    System.out.println("Data da Ultima Atualização: " + login.getDataAtualizacao());
+                    System.out.println("------------------------");
+                    System.out.println("Deseja prosseguir com a auteração: [Sim] / [Não]");
+                    scan.nextLine();
+                    String alterar = scan.nextLine().toLowerCase().trim();
+                    if(alterar.equals("sim")){
+                        do{
+                            System.out.println("Informe o novo nome: ");
+                            if(login.setNome(scan.nextLine())){
+                                continuar = true;
+                            }else System.out.println("Nome não pode ser vazio");
+                        }while(continuar != true);
+
+                        do{
+                            continuar = false;
+                            System.out.println("Informe o novo e-mail: ");
+                            if(login.setEmail(scan.nextLine())){
+                                continuar = true;
+                            }else System.out.println("email não pode ser vazio e deve conter \"@\"");
+                        }while (continuar != true);
+
+                        do{
+                            continuar = false;
+                            System.out.println("Informe a nova senha");
+                            if(login.setSenha(scan.nextLine())){
+                                continuar = true;
+                            }else System.out.println("Senha não pode ser vazia");
+                        }while (continuar != true);
+                        System.out.println("Digite a data de hoje: ");
+                        login.setDataAtualizacao(scan.nextLine());
+
+                        System.out.println("------------------------");
+                        System.out.println("--Alterado-com-sucesso--");
+                        System.out.println("------------------------");
+                        System.out.println("ID: " + login.getID());
+                        System.out.println("Nome: " + login.getNome());
+                        System.out.println("E-mail: " + login.getEmail());
+                        System.out.println("Senha: " + login.getSenha());
+                        System.out.println("Status: " + login.isStatus());
+                        System.out.println("Data Cadastro: " + login.getDataCadastro());
+                        System.out.println("Data da Ultima Atualização: " + login.getDataAtualizacao());
+                        System.out.println("------------------------\n");
+                    }else System.out.println("Cancelando...\n\n");
+                }
                 break;
-            case 5:
+            case 0:
                 System.out.println("Saindo");
                 break;
             default:

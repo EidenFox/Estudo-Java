91eca9be486d3e9bc785101a10d83f18d06ec432
EidenFox <3rcafe@gmail.com>
2025-09-22 21:34:34 -0300

    alterações locais antes de fetch

--- Gerenciador APS/src/dao/ListaTarefasDao.java
+++ Gerenciador APS/src/dao/ListaTarefasDao.java
@@ -1,5 +1,22 @@
 package dao;
 
-public class ListaTarefasDao {
+import model.ListaTarefas;
 
+import java.util.ArrayList;
+
+public class ListaTarefasDao {
+    public ArrayList<ListaTarefas> list;
+
+    public ListaTarefasDao(){
+        list = new ArrayList<>();
+        list.add(new ListaTarefas("Terminar o Código", "Termina logo este código", "Daniel", "Administrador", "Pra ontem"));
+
+    }
+
+    public ArrayList<ListaTarefas> getLista() {
+        return list;
+    }
+    public boolean inserirTarefa(ListaTarefas tarefa) {
+        return list.add(tarefa);
+    }
 }

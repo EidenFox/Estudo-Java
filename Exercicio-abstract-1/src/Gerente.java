43bdbd8b535047006c86a3c92e4281a4190436ed
Daniel Faculdade <you@example.com>
2025-09-24 22:17:56 -0300

    faculdade

---
+++ Exercicio-abstract-1/src/Gerente.java
@@ -0,0 +1,13 @@
+public class Gerente extends Funcionario{
+
+    public Gerente(String nome, String matricula, double salario_base) {
+        super(nome, matricula, salario_base);
+    }
+
+    @Override
+    double calcularSalario(){
+        return this.getSalario_base()*2;
+
+    }
+
+}

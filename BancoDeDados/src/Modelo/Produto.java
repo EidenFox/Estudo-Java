e258b30f41063d489977705d5383b9d86db44eab
Daniel Faculdade <you@example.com>
2025-09-25 22:29:05 -0300

    SQL em Java, Insert e select

---
+++ BancoDeDados/src/Modelo/Produto.java
@@ -0,0 +1,41 @@
+package Modelo;
+
+public class Produto {
+private String nome;
+private double preco;
+private int quantidade;
+
+    public Produto (){
+
+    }
+
+    public Produto (String nome, double preco, int quantidade){
+        this.setNome(nome);
+        this.setQuantidade(quantidade);
+        this.setPreco(preco);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}

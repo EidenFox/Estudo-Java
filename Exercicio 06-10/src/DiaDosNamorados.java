699f11515be14a5087e8fcafb33747d7af30d91c
Daniel Faculdade <you@example.com>
2025-10-08 22:33:57 -0300

    instance of

--- Exercicio 06-10/src/DiaDosNamorados.java
+++ Exercicio 06-10/src/DiaDosNamorados.java
@@ -1,11 +1,14 @@
 public class DiaDosNamorados extends CartaoWeb{
-    public  DiaDosNamorados (String destinatario){
-        super(destinatario);
+    public  DiaDosNamorados (String destinatario, String mensagem){
+        super(destinatario, mensagem);
         this.setDestinatario(destinatario);
+        this.setMensagem(mensagem);
     }
 
     @Override
     public void ShowMessage() {
+        System.out.println("--------------------");
         System.out.println("Feliz Dia dos Namorados "+ this.destinatario);
+        System.out.println("Mensagem: "+ this.mensagem);
     }
 }

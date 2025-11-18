c8d8f6cf50253d2be36cd202dab5378cbed8e845
Daniel Faculdade <you@example.com>
2025-11-17 21:59:52 -0300

    Terminar atividade 2 para entregar

--- Atividade/src/Exercicio1/exercicio1Main.java
+++ Atividade/src/Exercicio1/exercicio1Main.java
@@ -6,30 +6,41 @@ import java.util.List;
 public class exercicio1Main {
     public static void main(){
 
-        List<Empregado> empregados =  new ArrayList<>(){
-        };
+
         Chefe chefe = new Chefe();
         Comissionado comissionado = new Comissionado();
 
 
-        chefe.setNome("Robson");
-        chefe.setEndereço("Rua rua, 123");
-        chefe.setSalarioMensal(8334.12);
-        chefe.setBonus(150.00);
-        empregados.add(chefe);
+        List<Empregado> empregados = new ArrayList<>();
 
-        empregados.add(new Comissionado(1230.55, 78, 5, "Pedro", "Rua pedro 1"));
+// Comissionados
+        empregados.add(new Comissionado(1230.55, 78, 5, "Pedro", "Rua Pedro 1"));
+        empregados.add(new Comissionado(1500.00, 80, 6, "Maria", "Av. Central 45"));
+        empregados.add(new Comissionado(980.75, 60, 4, "João", "Rua das Flores 12"));
+        empregados.add(new Comissionado(2000.00, 90, 7, "Ana", "Rua Azul 33"));
+        empregados.add(new Comissionado(1750.00, 85, 6, "Bruno", "Rua Nova 66"));
+        empregados.add(new Comissionado(1600.00, 70, 5, "Patrícia", "Av. das Árvores 99"));
+        empregados.add(new Comissionado(1850.00, 95, 8, "Lucas", "Rua Sol 77"));
+        empregados.add(new Comissionado(1400.00, 65, 4, "Fernanda", "Av. Norte 21"));
 
+// Chefes
+        empregados.add(new Chefe(8334.12, 150.00, "Robson", "Rua Rua, 123"));
+        empregados.add(new Chefe(9500.00, 200.00, "Clara", "Av. Brasil 500"));
+        empregados.add(new Chefe(8700.50, 180.00, "Carlos", "Rua Verde 88"));
+        empregados.add(new Chefe(9100.00, 220.00, "Juliana", "Rua Oeste 55"));
+        empregados.add(new Chefe(8800.00, 190.00, "Ricardo", "Av. Leste 101"));
+        empregados.add(new Chefe(9900.00, 250.00, "Sofia", "Rua Centro 88"));
+        empregados.add(new Chefe(10200.00, 300.00, "Paulo", "Rua Sul 19"));
 
-
+        System.out.println("CHEFES: ");
         for (Empregado e : empregados){
-//            System.out.println("Nome: "+ e.getNome());
-//            System.out.println("Endereço: "+ e.getEndereço());
-//            System.out.println("Salario Total: "+ e.);
-//            e.calcularSalario();
             if (e instanceof Chefe){
                 e.calcularSalario();
             }
+        }
+        System.out.println("COMISSIONADOS: ");
+        for (Empregado e : empregados){
+
             if (e instanceof Comissionado){
                 e.calcularSalario();
             }
---
+++ Atividade/src/Exercicio2/Edicao.java
@@ -0,0 +1,40 @@
+package Exercicio2;
+
+public class Edicao {
+    private int NroEdi;
+    private int VolumeEdi;
+
+    public Edicao(int nroEdi, int volumeEdi) {
+        this.NroEdi = nroEdi;
+        this.VolumeEdi = volumeEdi;
+    }
+
+    public int getNroEdi() {
+        return NroEdi;
+    }
+
+    public void setNroEdi(int nroEdi) {
+        NroEdi = nroEdi;
+    }
+
+    public int getVolumeEdi() {
+        return VolumeEdi;
+    }
+
+    public void setVolumeEdi(int volumeEdi) {
+        VolumeEdi = volumeEdi;
+    }
+
+    public int Registrar(){
+    return 0;
+}
+
+public int Consultar (int NroEdi, int VolEdi){
+
+    return 0;
+}
+
+
+
+
+}
